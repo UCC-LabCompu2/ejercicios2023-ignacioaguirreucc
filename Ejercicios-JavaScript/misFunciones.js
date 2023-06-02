50625f9844fb96b44476ea0c8e4ef589b09bcf6c
aguirre ignacio facundo <2209590@ucc.edu.ar>
2023-06-02 00:03:40 -0300

    Operaciones matematicas

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -55,3 +55,31 @@ function MostrarOcultar(valorMO){
         document.getElementById("divMO").style.display = 'none';
     }
 }
+
+function calcularsuma(){
+    var num1,num2;
+    num1=document.getElementsByName("sum_num1")[0].value;
+    num2=document.getElementsByName("sum_num2")[0].value;
+    document.getElementsByName("sum_total")[0].value=Number(num1)+Number(num2);
+}
+
+function calcularresta(){
+    var num1,num2;
+    num1=document.getElementsByName("res_num1")[0].value;
+    num2=document.getElementsByName("res_num2")[0].value;
+    document.getElementsByName("res_total")[0].value=Number(num1)-Number(num2);
+}
+
+function calcularproducto() {
+    var num1, num2;
+    num1 = document.getElementsByName("sum_num1")[0].value;
+    num2 = document.getElementsByName("sum_num2")[0].value;
+    document.getElementsByName("sum_total")[0].value = Number(num1) + Number(num2);
+}
+
+function calculardiv() {
+    var num1, num2;
+    num1 = document.getElementsByName("sum_num1")[0].value;
+    num2 = document.getElementsByName("sum_num2")[0].value;
+    document.getElementsByName("sum_total")[0].value = Number(num1) + Number(num2);
+}
