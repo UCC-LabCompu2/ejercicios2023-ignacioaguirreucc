d24a2e87c58e58f0bc8fd3ab994dcdbce075c435
aguirre ignacio facundo <2209590@ucc.edu.ar>
2023-06-02 00:49:41 -0300

    Ejercicio innerHTML

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -67,28 +67,28 @@ function MostrarOcultar(valorMO){
 
 function calcularsuma(){
     var num1,num2;
-    num1=document.getElementsByName("sum_num1")[0].value;
-    num2=document.getElementsByName("sum_num2")[0].value;
-    document.getElementsByName("sum_total")[0].value=Number(num1)+Number(num2);
+    num1=Number(document.getElementsByName("sum_num1")[0].value);
+    num2=Number(document.getElementsByName("sum_num2")[0].value);
+    document.getElementsByName("sum_total")[0].innerHTML=num1+num2;
 }
 
 function calcularresta(){
     var num1,num2;
-    num1=document.getElementsByName("res_num1")[0].value;
-    num2=document.getElementsByName("res_num2")[0].value;
-    document.getElementsByName("res_total")[0].value=Number(num1)-Number(num2);
+    num1=Number(document.getElementsByName("res_num1")[0].value);
+    num2=Number(document.getElementsByName("res_num2")[0].value);
+    document.getElementsByName("res_total")[0].innerHTML=num1-num2;
 }
 
 function calcularproducto() {
     var num1, num2;
-    num1 = document.getElementsByName("sum_num1")[0].value;
-    num2 = document.getElementsByName("sum_num2")[0].value;
-    document.getElementsByName("sum_total")[0].value = Number(num1) + Number(num2);
+    num1 = Number(document.getElementsByName("mul_num1")[0].value);
+    num2 = Number(document.getElementsByName("mul_num2")[0].value);
+    document.getElementsByName("mul_total")[0].innerHTML = num1*num2;
 }
 
 function calculardiv() {
     var num1, num2;
-    num1 = document.getElementsByName("sum_num1")[0].value;
-    num2 = document.getElementsByName("sum_num2")[0].value;
-    document.getElementsByName("sum_total")[0].value = Number(num1) + Number(num2);
+    num1 = Number(document.getElementsByName("div_num1")[0].value);
+    num2 = Number(document.getElementsByName("div_num2")[0].value);
+    document.getElementsByName("div_total")[0].innerHTML = num1/num2;
 }
