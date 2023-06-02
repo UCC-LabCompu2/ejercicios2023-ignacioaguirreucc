c1a15a9fd649a4d86a8d3549bd8c175f308160c4
aguirre ignacio facundo <2209590@ucc.edu.ar>
2023-06-01 23:23:04 -0300

    Conversion radianes y grados

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -29,5 +29,20 @@ if (isNaN(valor)){
     document.lasUnidades.unid_pulgada.value= 36 * valor;
     document.lasUnidades.unid_pie.value= 3 * valor;
 }
+}
 
+function GraRad (id){
+    var grad, rad;
+
+    if (id=="grados"){
+    grad=document.getElementById("grados").value;
+    rad= (grad*Math.PI)/180;
+    }
+
+    else if (id=="radianes"){
+        rad=document.getElementById("radianes").value;
+        grad= (rad*180)/Math.PI
+    }
+        document.getElementById("grados").value = grad;
+        document.getElementById("radianes").value = rad;
 }
