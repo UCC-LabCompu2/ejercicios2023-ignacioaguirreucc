9151e2887adc96a1c0fc4536f742d70a3d70ac18
aguirre ignacio facundo <2209590@ucc.edu.ar>
2023-07-24 20:40:59 -0300

    canvas cuadticulado

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -124,3 +124,48 @@ function dibujarcircuad(){
     ctx.fill();
 
 }
+
+function dibujarcuadriculado(){
+    var canvas= document.getElementById("myCanvas");
+    var ctx= canvas.getContext("2d");
+
+    var alturamax=canvas.height;
+    var anchomax=canvas.width;
+
+    ctx.beginPath();
+    for(var i=0;i<alturamax;) {
+        ctx.moveTo(0, i);
+        ctx.lineTo(1000, i);
+        ctx.strokeStyle = "#3e67d9";
+        ctx.stroke();
+        i=i+20;
+    }
+    ctx.closePath();
+
+
+    ctx.beginPath();
+    for(var i=0;i<anchomax;) {
+        ctx.moveTo(i, 0);
+        ctx.lineTo(i, alturamax);
+        ctx.strokeStyle = "#3e67d9";
+        ctx.stroke();
+        i=i+20;
+    }
+    ctx.closePath();
+
+
+    ctx.beginPath();
+    ctx.moveTo(0,alturamax/2);
+    ctx.lineTo(anchomax, alturamax/2);
+    ctx.strokeStyle = "#ff010d";
+    ctx.stroke();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.moveTo(anchomax/2,0);
+    ctx.lineTo(anchomax/2, alturamax);
+    ctx.strokeStyle = "#ff010d";
+    ctx.stroke();
+    ctx.closePath();
+
+}
