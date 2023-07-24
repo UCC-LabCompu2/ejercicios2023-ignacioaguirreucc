25dd2cdb809ade0f423d32ea72cb5514354bacc7
aguirre ignacio facundo <2209590@ucc.edu.ar>
2023-07-24 20:05:43 -0300

    Cuadrado y circulo

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -107,3 +107,20 @@ function cargaresultado(){
     un=urlcomp.split("#")[2];
     document.getElementById("dist").value=can + " " + un;
 }
+
+function dibujarcircuad(){
+    var canvas=document.getElementById("myCanvas");
+    var ctx= canvas.getContext("2d");
+    var margen=5;
+    var ymax=canvas.height;
+    var xmax=canvas.width;
+
+    ctx.fillStyle="#333899";
+    ctx.fillRect(0+margen,ymax-40-margen,40,40);
+
+    ctx.arc(xmax/2,ymax/2,20,0,2*Math.PI);
+    ctx.stroke();
+    ctx.fillStyle="#33995f";
+    ctx.fill();
+
+}
